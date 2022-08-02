d859c61aa3c59964ae0aaacaa932a7c154dd2a2a
mirshohidmirvohidov <mirshohidmirvohidov@gmail.com>
2022-08-02 12:51:07 +0500

    change data/models.py/data_download

--- data/models.py
+++ data/models.py
@@ -18,3 +18,18 @@ class Data(BaseModel):
 
     def __str__(self):
         return self.title
+
+
+class DataDownload(BaseModel):
+    title = models.CharField(max_length=512, verbose_name='Mavzu')
+    file = models.FileField(upload_to='files/DataDownload', verbose_name='Fayl')
+    menu = TreeForeignKey(Menu, null=True, blank=True, related_name='data',
+                             on_delete=models.SET_NULL, verbose_name='Menyu')
+
+    class Meta:
+        db_table = 'data_download'
+        verbose_name = "Yuklanib olinuvchi ma'lumotnoma"
+        verbose_name_plural = "Yuklanib olinuvchi ma'lumotnomalar"
+
+    def __str__(self):
+        return self.title
